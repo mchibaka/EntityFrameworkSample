3f927974bfb8ed3052990139cdad7e3419079025
Michael Chibaka <mike@smartbitconsulting.com>
2025-06-14 17:22:19 +0200

    Add project files.

---
+++ EntityFramework-Insert/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_Insert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TRADITIONAL CRUD
+            InsertRecordSample(); //C
+            ReadRecordSample();   //R
+            UpdateRecordSample(); //U
+            DeleteRecordSample(); //D
+            
+            //ENTITY FRAMEWORK
+
+
+        }
+
+        #region TraditionalCRUD
+
+        private static void DeleteRecordSample()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;User Id=test;Password=test;Database=SampleDatabase;");
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "DELETE FROM users WHERE username = 'jchibaka'";
+            cmd.Connection = connection;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Record deleted successfully");
+        }
+
+        private static void UpdateRecordSample()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;User Id=test;Password=test;Database=SampleDatabase;");
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE users SET surname = 'Gondwe' WHERE username = 'jchibaka'";
+            cmd.Connection = connection;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Record updated successfully");
+        }
+
+        private static void ReadRecordSample()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;User Id=test;Password=test;Database=SampleDatabase;");
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT firstname, surname FROM users";
+            cmd.Connection = connection;
+            var reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                Console.WriteLine("Firstname : " + reader["firstname"]);
+                Console.WriteLine("Surname : " + reader["surname"]);
+            }
+            else
+            {
+                Console.WriteLine("No data found!");
+            }
+            Console.WriteLine("Record read successfully");
+        }
+
+        private static void InsertRecordSample()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;User Id=test;Password=test;Database=SampleDatabase;");
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Users ([Username],[Firstname],[Surname]," +
+                "[Password],[DateOfBirth],[Gender],[Remarks])" +
+                "VALUES('jchibaka','John','Chibaka'," +
+                "'testpassword123','1 January 1982','M'," +
+                "'Test user')";
+            cmd.Connection = connection;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Record inserted successfully");
+        }
+        #endregion
+
+    }
+}
---
+++ EntityFramework-Insert/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EntityFramework-Insert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EntityFramework-Insert")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a795f39f-de87-4a08-8b1d-f7ccc41d297c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
